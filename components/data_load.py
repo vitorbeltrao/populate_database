609614e919877c766feb5af52a012dc68619c658
vitorbeltrao <vitorbeltraoo@hotmail.com>
2023-04-27 18:28:56 -0300

    🎨 Finish data_load component

--- components/data_load.py
+++ components/data_load.py
@@ -1,5 +1,5 @@
 '''
-File to create the schemas, tables and populate 
+File to create the schemas, tables and populate
 them inside my postgres database
 
 Author: Vitor Abdo
@@ -11,36 +11,17 @@ import logging
 import psycopg2
 import pandas as pd
 from sqlalchemy import create_engine, inspect
-from decouple import config
-
-from data_collector import collect_raw_json_data
-from data_transform import transform_json_data
-
-from data_collector import collect_raw_csv_data
-from data_transform import transform_string_to_float
-from data_transform import transform_string_to_datetime
 
 logging.basicConfig(
     level=logging.INFO,
     filemode='w',
     format='%(name)s - %(levelname)s - %(message)s')
 
-# config
-DB_NAME = config('DB_NAME')
-USER = config('USER')
-PASSWORD = config('PASSWORD')
-SCHEMAS_TO_CREATE = config('SCHEMAS_TO_CREATE').split(',')
-OPEN_POSITIONS_RAW_PATH = config('OPEN_POSITIONS_RAW_PATH')
-NBA_PAYROLL_RAW_PATH = config('NBA_PAYROLL_RAW_PATH')
-NBA_PLAYER_BOX_RAW_PATH = config('NBA_PLAYER_BOX_RAW_PATH')
-NBA_PLAYER_STATS_RAW_PATH = config('NBA_PLAYER_STATS_RAW_PATH')
-NBA_SALARIES_RAW_PATH = config('NBA_SALARIES_RAW_PATH')
-
 
 def create_schema_into_postgresql(
-        db_name: str, 
-        user_name: str, 
-        password: str, 
+        db_name: str,
+        user_name: str,
+        password: str,
         schema_name: str) -> None:
     '''Connects to a PostgreSQL database and creates a schema if it does not already exist
 
@@ -50,7 +31,7 @@ def create_schema_into_postgresql(
     :param user_name: (str)
     The name of the user to authenticate as
 
-    :param password: (str) 
+    :param password: (str)
     The user's password
 
     :param schema_name: (str)
@@ -69,7 +50,8 @@ def create_schema_into_postgresql(
     cur = conn.cursor()
 
     # Execute a query to check if the schema already exists
-    cur.execute(f"SELECT schema_name FROM information_schema.schemata WHERE schema_name = '{schema_name}'")
+    cur.execute(
+        f"SELECT schema_name FROM information_schema.schemata WHERE schema_name = '{schema_name}'")
     result = cur.fetchone()
 
     # If the schema does not exist, create it
@@ -87,10 +69,10 @@ def create_schema_into_postgresql(
 
 def create_table_into_postgresql(
         db_name: str,
-        user_name: str, 
-        password: str, 
-        schema_name: str, 
-        table_name: str, 
+        user_name: str,
+        password: str,
+        schema_name: str,
+        table_name: str,
         table_columns: str) -> None:
     '''Function that creates a table if it does not exist in a PostgresSQL schema
 
@@ -100,7 +82,7 @@ def create_table_into_postgresql(
     :param user_name: (str)
     The name of the user to authenticate as
 
-    :param password: (str) 
+    :param password: (str)
     The user's password
 
     :param schema_name: (str)
@@ -126,7 +108,7 @@ def create_table_into_postgresql(
 
     # Check if the table exists in the schema
     cur.execute(
-        'SELECT EXISTS(SELECT * FROM information_schema.tables WHERE table_schema = %s AND table_name = %s)', 
+        'SELECT EXISTS(SELECT * FROM information_schema.tables WHERE table_schema = %s AND table_name = %s)',
         (schema_name, table_name))
     exists = cur.fetchone()[0]
 
@@ -134,9 +116,11 @@ def create_table_into_postgresql(
     if not exists:
         create_table_query = f'CREATE TABLE {schema_name}.{table_name} ({table_columns})'
         cur.execute(create_table_query)
-        logging.info(f'The table {table_name} was created in the {schema_name} schema')
+        logging.info(
+            f'The table {table_name} was created in the {schema_name} schema')
     else:
-        logging.info(f'The table {table_name} already exists in the {schema_name} schema')
+        logging.info(
+            f'The table {table_name} already exists in the {schema_name} schema')
 
     # Commit changes and close the connection
     conn.commit()
@@ -145,12 +129,12 @@ def create_table_into_postgresql(
 
 
 def insert_data_into_postgresql(
-    datab_name: str,
-    user_name: str, 
-    password: str, 
-    schema_name: str, 
-    table_name: str, 
-    df: pd.DataFrame) -> None:
+        datab_name: str,
+        user_name: str,
+        password: str,
+        schema_name: str,
+        table_name: str,
+        df: pd.DataFrame) -> None:
     '''
     Function that inserts data from a Pandas DataFrame into a PostgreSQL table.
     If the table does not exist, it creates a new one in the specified schema.
@@ -161,7 +145,7 @@ def insert_data_into_postgresql(
     :param user_name: (str)
     The name of the user to authenticate as.
 
-    :param password: (str) 
+    :param password: (str)
     The user's password.
 
     :param schema_name: (str)
@@ -189,11 +173,17 @@ def insert_data_into_postgresql(
         password=db_pass
     )
     # create engine
-    engine = create_engine(f'postgresql+psycopg2://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}')
+    engine = create_engine(
+        f'postgresql+psycopg2://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}')
 
     # Create a temporary table with the data from the DataFrame
     temp_table_name = f'temp_{table_name}'
-    df.to_sql(name=temp_table_name, con=engine.connect(), schema=schema_name, index=False, if_exists='replace')
+    df.to_sql(
+        name=temp_table_name,
+        con=engine.connect(),
+        schema=schema_name,
+        index=False,
+        if_exists='replace')
     logging.info('Temporary table was created: SUCCESS')
 
     # Check if the final table exists
@@ -210,7 +200,8 @@ def insert_data_into_postgresql(
         df_columns = df.columns.tolist()
 
         if db_columns != df_columns:
-            raise ValueError(f'The columns of the DataFrame do not match the columns of the table {schema_name}.{table_name}')
+            raise ValueError(
+                f'The columns of the DataFrame do not match the columns of the table {schema_name}.{table_name}')
 
         # Insert the data into the final table without overwriting existing data
         insert_query = f'INSERT INTO {schema_name}.{table_name} SELECT * FROM {schema_name}.{temp_table_name} ON CONFLICT DO NOTHING;'
@@ -227,264 +218,3 @@ def insert_data_into_postgresql(
     # Close the database connection
     conn.commit()
     conn.close()
-
-
-if __name__ == "__main__":
-    # 1. create the schema if it does not already exist
-    logging.info('About to start executing the create schema function')
-    for schema in SCHEMAS_TO_CREATE:
-        create_schema_into_postgresql(DB_NAME, USER, PASSWORD, schema)
-    logging.info('Done executing the create schema function\n')
-
-    # 2. create tables
-    # 2.1 create first table in "startups_hiring" schema
-    logging.info('About to start executing the create table "open_positions" function')
-    table_columns = '''
-    company_name VARCHAR(50),
-    headline TEXT,
-    tags TEXT,
-    website TEXT,
-    employees VARCHAR(50),
-    about TEXT,
-    locations TEXT,
-    industries TEXT,
-    engineering INT,
-    founder INT,
-    investor INT,
-    marketing INT,
-    other_engineering INT,
-    product INT,
-    sales INT,
-    designer INT,
-    management INT,
-    operations INT
-    '''
-    create_table_into_postgresql(DB_NAME, USER, PASSWORD, 'startups_hiring', 'open_positions', table_columns)
-    logging.info('Done executing the create table "open_positions" function\n')
-
-    # 2.2 create first table in "nba" schema
-    logging.info('About to start executing the create table "nba_payroll" function')
-    table_columns = '''
-    team VARCHAR(30),
-    season_start_year INT,
-    payroll FLOAT,
-    inflation_adj_payroll FLOAT
-    '''
-    create_table_into_postgresql(DB_NAME, USER, PASSWORD, 'nba', 'nba_payroll', table_columns)
-    logging.info('Done executing the create table "nba_payroll" function\n')
-
-    # 2.2 create second table in "nba" schema
-    logging.info('About to start executing the create table "player_box_score_stats" function')
-    table_columns = '''
-    season INT,
-    game_id INT,
-    player_name VARCHAR(30),
-    team VARCHAR(30),
-    game_date DATE,
-    matchup VARCHAR(20),
-    wl VARCHAR (5),
-    min INT,
-    fgm INT,
-    fga FLOAT,
-    fg_pct FLOAT,
-    fg3m FLOAT,
-    fg3a FLOAT,
-    fg3_pct FLOAT,
-    ftm INT,
-    fta FLOAT,
-    ft_pct FLOAT,
-    oreb FLOAT,
-    dreb FLOAT,
-    reb FLOAT,
-    ast FLOAT,
-    stl FLOAT,
-    blk FLOAT,
-    tov FLOAT,
-    pf FLOAT,
-    pts INT,
-    plus_minus FLOAT,
-    video_available INT
-    '''
-    create_table_into_postgresql(DB_NAME, USER, PASSWORD, 'nba', 'player_box_score_stats', table_columns)
-    logging.info('Done executing the create table "player_box_score_stats" function\n')
-
-    # 2.3 create third table in "nba" schema
-    logging.info('About to start executing the create table "player_stats" function')
-    table_columns = '''
-    season INT,
-    player_name VARCHAR(30),
-    pos VARCHAR(10),
-    age INT,
-    tm VARCHAR(10),
-    g FLOAT,
-    gs FLOAT,
-    mp FLOAT,
-    fg FLOAT,
-    fga FLOAT,
-    fg_percent FLOAT,
-    threep FLOAT,
-    threepa FLOAT,
-    threep_percent FLOAT,
-    twop FLOAT,
-    twopa FLOAT,
-    twop_percent FLOAT,
-    efg_percent FLOAT,
-    ft FLOAT,
-    fta FLOAT,
-    ft_percent FLOAT,
-    orb FLOAT,
-    drb FLOAT,
-    trb FLOAT,
-    ast FLOAT,
-    stl FLOAT,
-    blk FLOAT,
-    tov FLOAT,
-    pf FLOAT,
-    pts FLOAT
-    '''
-    create_table_into_postgresql(DB_NAME, USER, PASSWORD, 'nba', 'player_stats', table_columns)
-    logging.info('Done executing the create table "player_stats" function\n')
-
-    # 2.4 create fourth table in "nba" schema
-    logging.info('About to start executing the create table "nba_salaries" function')
-    table_columns = '''
-    player_name VARCHAR(30),
-    season_start_year INT,
-    salary FLOAT,
-    inflation_adj_salary FLOAT
-    '''
-    create_table_into_postgresql(DB_NAME, USER, PASSWORD, 'nba', 'nba_salaries', table_columns)
-    logging.info('Done executing the create table "nba_salaries" function\n')
-
-    # 3. insert transformed dataframes into postgres
-    ### 3.1 insert data into open_positions table
-    logging.info('About to start inserting the data into open_positions table')
-
-    # extracting data
-    open_positions_raw_df = collect_raw_json_data(OPEN_POSITIONS_RAW_PATH)
-
-    # transforming data
-    columns_to_drop = ['id', 'logo_url']
-    columns_to_convert_to_str = ['tags', 'locations', 'industries']
-    open_positions_transformed_df = transform_json_data(
-        open_positions_raw_df, columns_to_drop, columns_to_convert_to_str, 'jobs')
-    
-    open_positions_transformed_df = open_positions_transformed_df.rename(
-        columns=lambda x: x.strip().lower().replace(' ', '_')) # standardize column names
-    
-    open_positions_transformed_df.drop_duplicates(inplace=True)
-
-    # loading data
-    insert_data_into_postgresql(
-        DB_NAME, USER, PASSWORD, 'startups_hiring', 'open_positions', open_positions_transformed_df)
-    logging.info('Done executing inserting the data into open_positions table\n')
-
-    ### 3.2 insert data into nba_payroll table
-    logging.info('About to start inserting the data into nba_payroll table')
-
-    # extracting data
-    nba_payroll_raw_df = collect_raw_csv_data(NBA_PAYROLL_RAW_PATH)
-
-    # transforming data
-    columns_to_convert_to_float = ['payroll', 'inflationAdjPayroll']
-    nba_payroll_transformed_df = transform_string_to_float(
-        nba_payroll_raw_df, columns_to_convert_to_float)
-    
-    nba_payroll_transformed_df.drop(['Unnamed: 0'], axis=1, inplace=True) # drop unnecessary columns
-
-    nba_payroll_transformed_df = nba_payroll_transformed_df.rename(
-        columns=lambda x: x.strip().lower().replace(' ', '_')) # standardize column names
-    nba_payroll_transformed_df.rename(
-        columns={
-        'seasonstartyear': 'season_start_year', 
-        'inflationadjpayroll': 'inflation_adj_payroll'}, inplace=True) # standardize column names
-    
-    nba_payroll_transformed_df.drop_duplicates(inplace=True)
-
-    # loading data
-    insert_data_into_postgresql(
-        DB_NAME, USER, PASSWORD, 'nba', 'nba_payroll', nba_payroll_transformed_df)
-    logging.info('Done executing inserting the data into nba_payroll table\n')
-
-    ### 3.3 insert data into player_box_score_stats table
-    logging.info('About to start inserting the data into player_box_score_stats table')
-
-    # extracting data
-    nba_player_box_raw_df = collect_raw_csv_data(NBA_PLAYER_BOX_RAW_PATH)
-
-    # transforming data
-    column_to_convert_to_date = 'GAME_DATE'
-    nba_player_box_transformed_df = transform_string_to_datetime(
-        nba_player_box_raw_df, column_to_convert_to_date)
-    
-    nba_player_box_transformed_df.drop(['Unnamed: 0'], axis=1, inplace=True) # drop unnecessary columns
-
-    nba_player_box_transformed_df = nba_player_box_transformed_df.rename(
-        columns=lambda x: x.strip().lower().replace(' ', '_')) # standardize column names
-
-    nba_player_box_transformed_df.drop_duplicates(inplace=True)
-
-    # loading data
-    insert_data_into_postgresql(
-        DB_NAME, USER, PASSWORD, 'nba', 'player_box_score_stats', nba_player_box_transformed_df)
-    logging.info('Done executing inserting the data into player_box_score_stats table\n')
-
-    ### 3.4 insert data into player_stats table
-    logging.info('About to start inserting the data into player_stats table')
-
-    # extracting data
-    nba_player_stats_raw_df = collect_raw_csv_data(NBA_PLAYER_STATS_RAW_PATH)
-
-    # transforming data
-    nba_player_stats_transformed_df = nba_player_stats_raw_df.drop(
-        ['Unnamed: 0.1', 'Unnamed: 0'], axis=1) # drop unnecessary columns
-
-    nba_player_stats_transformed_df = nba_player_stats_transformed_df.rename(
-        columns=lambda x: x.strip().lower().replace(' ', '_')) # standardize column names
-    nba_player_stats_transformed_df.rename(
-        columns={
-        'player': 'player_name', 
-        'fg%': 'fg_percent',
-        '3p': 'threep',
-        '3pa': 'threepa',
-        '3p%': 'threep_percent',
-        '2p': 'twop',
-        '2pa': 'twopa',
-        '2p%': 'twop_percent',
-        'efg%': 'efg_percent',
-        'ft%': 'ft_percent'}, inplace=True) # standardize column names
-    
-    nba_player_stats_transformed_df.drop_duplicates(inplace=True)
-
-    # loading data
-    insert_data_into_postgresql(
-        DB_NAME, USER, PASSWORD, 'nba', 'player_stats', nba_player_stats_transformed_df)
-    logging.info('Done executing inserting the data into player_stats table\n')
-
-    ### 3.5 insert data into nba_salaries table
-    logging.info('About to start inserting the data into nba_salaries table')
-
-    # extracting data
-    nba_salaries_raw_df = collect_raw_csv_data(NBA_SALARIES_RAW_PATH)
-
-    # transforming data
-    columns_to_convert_to_float = ['salary', 'inflationAdjSalary']
-    nba_salaries_transformed_df = transform_string_to_float(
-        nba_salaries_raw_df, columns_to_convert_to_float)
-    
-    nba_salaries_transformed_df.drop(['Unnamed: 0'], axis=1, inplace=True) # drop unnecessary columns
-
-    nba_salaries_transformed_df = nba_salaries_transformed_df.rename(
-        columns=lambda x: x.strip().lower().replace(' ', '_')) # standardize column names
-    nba_salaries_transformed_df.rename(
-        columns={
-        'playername': 'player_name', 
-        'seasonstartyear': 'season_start_year',
-        'inflationadjsalary': 'inflation_adj_salary'}, inplace=True) # standardize column names
-    
-    nba_salaries_transformed_df.drop_duplicates(inplace=True)
-
-    # loading data
-    insert_data_into_postgresql(
-        DB_NAME, USER, PASSWORD, 'nba', 'nba_salaries', nba_salaries_transformed_df)
-    logging.info('Done executing inserting the data into nba_salaries table\n')
